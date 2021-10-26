a75981d2dc7652dbba6189c06f2b8de31565f4d9
sollametr <kuba.andrey.jr@yandex.ru>
2021-10-26 11:22:31 +0300

    Дабавление перехода между окнами

--- main_files/main.py
+++ main_files/main.py
@@ -1,6 +1,5 @@
 import sys
 
-
 from PyQt5 import uic  # Импортируем uic
 from PyQt5.QtWidgets import QApplication, QMainWindow
 
@@ -10,11 +9,13 @@ class MyWidget(QMainWindow):
         super().__init__()
         uic.loadUi('authorization.ui', self)
         self.pushButton.clicked.connect(self.password)
-        self.pushButton_2.clicked.connect(self.password)
+        self.pushButton_2.clicked.connect(self.registr)
 
     def password(self):
         uic.loadUi('main_window.ui', self)
 
+    def registr(self):
+        uic.loadUi('registration.ui', self)
 
 
 if __name__ == '__main__':
