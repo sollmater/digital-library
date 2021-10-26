b03f3249f47b42fddbb62709fe45bb2113bd8ea3
sollametr <kuba.andrey.jr@yandex.ru>
2021-10-26 11:18:14 +0300

    Создание окна входа в приложение с возможностью регистрации

--- main_files/main.py
+++ main_files/main.py
@@ -1,13 +1,24 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import sys
 
 
-def help(a):
-    print(a)
+from PyQt5 import uic  # Импортируем uic
+from PyQt5.QtWidgets import QApplication, QMainWindow
 
-print('Hello World')
-#uihdwedwedewde
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+class MyWidget(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('authorization.ui', self)
+        self.pushButton.clicked.connect(self.check_password)
+        self.pushButton_2.clicked.connect(self.check_password)
+
+    def check_password(self):
+        uic.loadUi('main_window.ui', self)
+
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = MyWidget()
+    ex.show()
+    sys.exit(app.exec_())
